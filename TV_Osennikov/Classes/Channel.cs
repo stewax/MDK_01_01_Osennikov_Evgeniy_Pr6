931bd999cfdfa9f68b13fcefca6f2fc7889cbe7d
Anna <aneckalarionova@gmail.com>
2025-09-29 09:12:20 +0500

    Разработанный класс Channel. Osennikov

---
+++ TV_Osennikov/Classes/Channel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV_Osennikov.Classes
+{
+    public class Channel
+    {
+        public string Name { get; set; }
+        public string Src { get; set; }
+    }
+}
